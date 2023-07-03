e61b843ca9ea6b07a3b3a18ee099e1b308aaadae
KaustavPal <kaustavpal95@gmail.com>
2023-07-03 12:36:33 +0530

    Now it can store multiple user data

--- Task_7/app.js
+++ Task_7/app.js
@@ -1,6 +1,8 @@
 const myForm=document.querySelector("#my-form");
 const nameInput= document.querySelector("#name");
 const emailInput= document.querySelector("#email");
+const phoneInput= document.querySelector("#phone");
+const ul=document.querySelector("#user-info");
 
 
 myForm.addEventListener("submit", onSubmit);
@@ -8,9 +10,15 @@ function onSubmit(e){
     e.preventDefault();
     let userObj = {
         name: nameInput.value,
-        email: emailInput.value
+        email: emailInput.value,
+        phone: phoneInput.value
     };
     let userObj_serialized = JSON.stringify(userObj);
-    console.log(userObj_serialized);
-    localStorage.setItem("user", userObj_serialized);   
+    localStorage.setItem(emailInput.value, userObj_serialized); 
+    let userObj_deserialized = JSON.parse(localStorage.getItem(emailInput.value));
+    let li = document.createElement("li");
+    let newUser = `${userObj_deserialized.name} - ${userObj_deserialized.email} - ${userObj_deserialized.phone}`;
+    li.appendChild(document.createTextNode(newUser));
+    ul.appendChild(li);  
 }
+
