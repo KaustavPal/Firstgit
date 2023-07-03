ed94222ea74a9a6d41688f9f87eaf0bde0b814e4
KaustavPal <kaustavpal95@gmail.com>
2023-07-03 10:48:41 +0530

    Storing user details as object

--- Task_7/app.js
+++ Task_7/app.js
@@ -3,17 +3,15 @@ const msg=document.querySelector(".msg");
 const nameInput= document.querySelector("#name");
 const emailInput= document.querySelector("#email");
 
+
 myForm.addEventListener("submit", onSubmit);
 function onSubmit(e){
     e.preventDefault();
-    if(nameInput.value=== "" || emailInput.value===""){
-        msg.classList.add("error");
-        msg.innerHTML="Please enter all fields";
-
-        setTimeout(() => msg.remove(), 3000);
-    }
-    else{
-        localStorage.setItem(emailInput.value, nameInput.value);
-    }
-    
+    let userObj = {
+        name: nameInput.value,
+        email: emailInput.value
+    };
+    let userObj_serialized = JSON.stringify(userObj);
+    console.log(userObj_serialized);
+    localStorage.setItem("user", userObj_serialized);   
 }
